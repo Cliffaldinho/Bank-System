647890464d1d0c0b35d05a0dcd3017a3dfb6d635
Cliffaldinho <c3134188@uon.edu.au>
2019-05-20 00:38:20 +1000

    Add files via upload

---
+++ data/PerformAnalysisServlet.java
@@ -0,0 +1,48 @@
+package data;
+
+import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.*;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.*;
+
+@WebServlet(urlPatterns={"/performAnalysis"})
+public class PerformAnalysisServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException,ServletException {
+		
+		PrintWriter out = res.getWriter();
+		
+		int storeIndex;
+		String indexParameter;
+		
+		indexParameter=(String) req.getParameter("indexForAnalysis");
+		storeIndex=Integer.parseInt(indexParameter);
+
+	
+		//An Analysis object created
+		Analysis anAnalysis = new Analysis();
+		
+		//Added an Incident to the Analysis Object
+		anAnalysis.setOneIncident(IncidentDatabase.getIncidentsList().get(storeIndex));
+		
+		//Stored the Analysis in the Analysis database
+		AnalysisDatabase.addAnalysis(anAnalysis);
+		
+		IncidentDatabase.getIncidentsList().get(storeIndex).setHasAnalysis(true);
+		
+		int sizeOfList=AnalysisDatabase.getAnalysisList().size();
+		int mostRecentIndex=sizeOfList-1;
+		IncidentDatabase.getIncidentsList().get(storeIndex).setIndexInAnalysisDatabase(mostRecentIndex);
+		
+		req.setAttribute("incidentDatabaseIndex",storeIndex);
+		req.setAttribute("analysisDatabaseIndex", mostRecentIndex);
+		
+		req.getRequestDispatcher("PerformAnalysis.jsp").forward(req,res);
+
+	}
+	
+
+}
