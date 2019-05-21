657327976b13601ff199bb9e368a49406d1a39ee
ZWelsh <50762478+ZWelsh@users.noreply.github.com>
2019-05-21 19:55:56 +1000

    Actually fixed error message
    As seen in last commitment.

--- data/UserLoginServlet.java
+++ data/UserLoginServlet.java
@@ -8,9 +8,6 @@ import java.io.PrintWriter;
 import java.io.IOException;
 import java.util.*;
 
-import javax.swing.JOptionPane;
-import javax.swing.JOptionPane.*;
-
 //receives from UserLogin.jsp
 
 
@@ -23,12 +20,10 @@ public class UserLoginServlet extends HttpServlet {
 		String password = req.getParameter("password");
 		User loginUser = new User();
 		boolean found = false;
-		User userTest = new User("Bob", "11 Test St", "00000000", User.Position.IT, "id1234", "password");
-		UserDatabase.addUsers(userTest);
 		
-		for (int i = 0; i < UserDatabase.getUsersList().size(); i++){
-			String id = UserDatabase.getUsersList().get(i).getStaffID();
-			String pw = UserDatabase.getUsersList().get(i).getPassword();
+		for (int i = 0; i < UserDatabase.getUserList().size(); i++){
+			id = UserDatabase.getUserList().get(i).getStaffID();
+			pw = UserDatabase.getUserList().get(i).getPassword();
 			if (userid.equals(id)){
 				if (password.equals(pw)){
 					found = true;
@@ -37,7 +32,7 @@ public class UserLoginServlet extends HttpServlet {
 			}
 		}	
 		if (!found){
-			JOptionPane.showMessageDialog(null, "Login Fail", "Incorrect username or password. Please try again.", JOptionPane.ERROR_MESSAGE);
+			req.setAttribute("loginError","Incorrect user ID or password.");
 			req.getRequestDispatcher("UserLogin.jsp").forward(req,res);
 		}
 	}
@@ -50,4 +45,4 @@ public class UserLoginServlet extends HttpServlet {
 	
 	
 	
-} 
+}
